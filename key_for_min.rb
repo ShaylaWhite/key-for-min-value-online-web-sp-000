95aa512a55926ba8df2eae02e1667e5dce78d5e0
Shayla White <shayladwhite1@gmail.com>
2019-05-27 23:58:47 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -12,7 +12,8 @@ def key_for_min_value(name_hash)
     end
     min_num = array[0]
     array.each do |value|
-      min_num = value if value < min_num
+      min_num = value
+      if value < min_num
     end
     name_hash.each do |key, value|
       if min_num == value
